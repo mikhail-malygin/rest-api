f999e7f74aab861299aacaf3c04627b458430c41
Mikhail Malygin <mihail.malygin@gmail.com>
2022-07-21 17:59:21 +0500

    added first tests

---
+++ src/test/java/qa/guru/SelenoidStatusTests.java
@@ -0,0 +1,66 @@
+package qa.guru;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+public class SelenoidStatusTests {
+
+    @Test
+    void checkTotal() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithGiven() {
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithLogs() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkChromeVersion() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+
+    @Test
+    void checkTotalSomeLogs() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+}
