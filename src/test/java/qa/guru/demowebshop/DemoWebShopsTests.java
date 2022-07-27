41c59c91c8fb47fac7384816b9fed5c53aa0a984
Mikhail Malygin <mihail.malygin@gmail.com>
2022-07-27 12:17:45 +0500

    Added Allure

--- src/test/java/qa/guru/demowebshop/DemoWebShopsTests.java
+++ src/test/java/qa/guru/demowebshop/DemoWebShopsTests.java
@@ -3,6 +3,9 @@ package qa.guru.demowebshop;
 import com.codeborne.selenide.Configuration;
 
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -11,19 +14,27 @@ import org.openqa.selenium.Cookie;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static helpers.CustomApiListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class DemoWebShopsTests {
 
+    String authCookieName = "NOPCOMMERCE.AUTH",
+           email = "vbdv@feferf.ru",
+           password = "itLf7@U@Bf6khGH";
+
     @BeforeAll
     static void setUp() {
         Configuration.baseUrl = "http://demowebshop.tricentis.com";
         RestAssured.baseURI = "http://demowebshop.tricentis.com";
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @Test
-    void addNewProductToCartAsAnonymTest() {
+    void addNewToCartAsAnonymTest() {
 
         String body = "product_attribute_72_5_18=53" +
                 "&product_attribute_72_6_19=54" +
@@ -44,7 +55,7 @@ public class DemoWebShopsTests {
     }
 
     @Test
-    void addOldProductToCartAsAnonymTest() {
+    void addToCartAsAnonymTest() {
         /*
         curl 'http://demowebshop.tricentis.com/addproducttocart/details/72/1' \
         -H 'Accept: *\/*' \
@@ -80,7 +91,7 @@ public class DemoWebShopsTests {
     }
 
     @Test
-    void addOldProductToCartAsAuthorizedTest() {
+    void addToCartAsAuthorizedTest() {
 
         //-H 'Cookie: Nop.customer=6f285aa7-2c0f-4e58-82cb-03a039ff0b90;
         // NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=72&RecentlyViewedProductIds=31;  __RequestVerificationToken=k03NLbscWhyjcHuegyzssXL9TCpkX9Aj16BS0_wi7Vn_D_LCs9lL4lWqlynvtDGQmlzF6zRel6CnDFhVQ3yCfm01cQSaDu4RNI2_Jl9YsP41; NOPCOMMERCE.AUTH=80858C6C75C772B6E10B8D966ED4E34D909DF3084CDF054F72FA834F6E2344D0B7C6CD8195F37361E387BF5DBC7C9344B829E023B00B8CD301F69D0556FBE63745919893FDF76ED9CD518126061E476770B70C42157B8839AE65B1E2EAD754E11FABD66DB6FA85A228F5C2FB1B5B39D7F9CCD1571232FDBEE75B693675899066FA8ED55635F98A6AD00E6ED6B7E6F7C0; Nop.customer=bccc3d3b-96f1-4e02-aef5-52bd4ae25d47;
@@ -116,13 +127,12 @@ public class DemoWebShopsTests {
     }
 
     @Test
-    void addOldProductToCartAsAuthorizedSizeInWebTest() {
+    void addToCartAsAuthorizedSizeInWebTest() {
 
-        String authCookieName = "NOPCOMMERCE.AUTH";
         String authCookieValue = given()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .formParam("Email", "vbdv@feferf.ru")
-                .formParam("Password","itLf7@U@Bf6khGH")
+                .formParam("Email", email)
+                .formParam("Password",password)
                 .when()
                 .post("/login")
                 .then()
@@ -157,5 +167,66 @@ public class DemoWebShopsTests {
 
         open("");
         $(".cart-qty").shouldHave(text(cartSize));
+    }
+
+    @Test
+    void addToCartWithAllureTest() {
+
+        String authCookieValue = getAuthCookie(email, password);
+
+        String body = "product_attribute_72_5_18=53" +
+                "&product_attribute_72_6_19=54" +
+                "&product_attribute_72_3_20=57" +
+                "&addtocart_72.EnteredQuantity=1";
+
+        String cartSize = getCartSize(body, authCookieValue);
+
+        step("Open minimal content, because cookie can be set when site is opened", () ->
+                open("/Themes/DefaultClean/Content/images/logo.png"));
+
+        step("Set cookie to to browser", () -> {
+            Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+            WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Check cart size", () ->
+                $(".cart-qty").shouldHave(text(cartSize)));
+    }
+
+    @Step("Get authorization cookie")
+    String getAuthCookie(String email, String password) {
+        return given()
+                .filter(withCustomTemplates())
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .formParam("Email", email)
+                .formParam("Password",password)
+                .when()
+                .post("/login")
+                .then()
+                .log().body()
+                .log().cookies()
+                .statusCode(302)
+                .extract()
+                .cookie(authCookieName);
+    }
+
+    @Step("Get cart size")
+    String getCartSize(String body, String authCookieValue) {
+        return given()
+                .filter(withCustomTemplates())
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie(authCookieName, authCookieValue)
+                .body(body)
+                .when()
+                .post("/addproducttocart/details/72/1")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message",is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
+                .extract()
+                .path("updatetopcartsectionhtml");
     }
 }
